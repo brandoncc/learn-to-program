0ca317e263f0c4582d156b59749d3a0f941394a6
Brandon Conway <brandoncc@gmail.com>
2013-12-01 17:56:29 -0800

    7.4 modifications

--- chapter_7/7.4.rb
+++ chapter_7/7.4.rb
@@ -1,7 +1,25 @@
-puts 'Hello, what\'s your name?'
-name = gets.chomp
-puts 'Hello, ' + name + '.'
+# puts 'Hello, what\'s your name?'
+# name = gets.chomp
+# puts 'Hello, ' + name + '.'
 
-if name == 'Chris' || name == 'Katy'
-  puts 'What a lovely name'
-end
+# if name == 'Chris' || name == 'Katy'
+#   puts 'What a lovely name'
+# end
+
+i_am_chris  = true
+i_am_purple = false
+i_like_beer = true
+i_eat_rocks = false
+
+puts i_am_chris  && i_like_beer
+puts i_like_beer && i_eat_rocks
+puts i_am_purple && i_like_beer
+puts i_am_purple && i_eat_rocks
+puts
+puts i_am_chris  || i_like_beer
+puts i_like_beer || i_eat_rocks
+puts i_am_purple || i_like_beer
+puts i_am_purple || i_eat_rocks
+puts
+puts !i_am_purple
+puts !i_am_chris
